6e9d42c95b329982e75924ac05651565f9d004ba
CristiHE <92540063+CristiHE@users.noreply.github.com>
2021-10-19 23:35:24 +0300

    update application

--- src/main/java/com/sda/project/Application.java
+++ src/main/java/com/sda/project/Application.java
@@ -1,5 +1,9 @@
 package com.sda.project;
 
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         System.out.println("Test");
